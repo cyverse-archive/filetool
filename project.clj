e8da9bcb3737232b652ce33aca463a9f12acf8b0
Dennis Roberts <dennis@iplantcollaborative.org>
2012-04-17 17:36:37 -0700

    CORE-3104: use lein-iplant-rpm to generate RPM files

--- project.clj
+++ project.clj
@@ -1,9 +1,17 @@
 (defproject porklock "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A command-line tool for interacting with iRODS."
   :main porklock.core
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
                  [org.clojure/tools.cli "0.2.1"]
                  [commons-io/commons-io "2.2"]
-                 [slingshot "0.10.2"]])
+                 [slingshot "0.10.2"]]
+  :dev-dependencies [[org.iplantc/lein-iplant-rpm "1.1.0-SNAPSHOT"]]
+  :iplant-rpm {:summary "Porklock"
+               :type :command
+               :release 1
+               :provides "iplant-porklock"
+               :exe-files ["curl_wrapper.pl"]}
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
