8d7deeff91d68c3153e1b744dc36358bff485fee
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-04-19 09:55:51 -0700

    Changed the release.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :dev-dependencies [[org.iplantc/lein-iplant-rpm "1.1.0-SNAPSHOT"]]
   :iplant-rpm {:summary "Porklock"
                :type :command
-               :release 1
+               :release 2
                :exe-files ["curl_wrapper.pl"]}
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
