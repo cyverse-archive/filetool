f2407b14e982530886c6468e5f52deb7d9f20588
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-31 14:48:33 -0700

    CORE-3929: switched to the latest version of iplant-clojure-commons

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :main porklock.core
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]
                  [org.clojure/tools.cli "0.2.1"]
                  [commons-io/commons-io "2.2"]
                  [slingshot "0.10.2"]]
