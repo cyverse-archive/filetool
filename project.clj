5f221dfcccfc63e180b70b8d3154f42c0ca3da61
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-07 17:07:27 -0700

    CORE-5133: removed all snapshot dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/porklock "1.2.2-SNAPSHOT"
+(defproject org.iplantc/porklock "1.2.2"
   :description "A command-line tool for interacting with iRODS."
   :url "http://www.iplantcollaborative.org"
   :license {:name "BSD"
@@ -12,15 +12,15 @@
   :main porklock.core
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.iplantc/clojure-commons "1.4.8-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.8"]
                  [org.clojure/tools.cli "0.2.1"]
                  [commons-io/commons-io "2.2"]
                  [slingshot "0.10.3"]
                  [com.cemerick/url "0.0.7"]
                  [com.novemberain/welle "1.4.0"]
                  [cheshire "5.1.1"]
-                 [org.iplantc/clj-jargon "0.4.2-SNAPSHOT"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.4.3-SNAPSHOT"]]
+                 [org.iplantc/clj-jargon "0.4.2"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.4.3"]]
   :iplant-rpm {:summary "Porklock"
                :type :command
                :exe-files ["curl_wrapper.pl"]}
